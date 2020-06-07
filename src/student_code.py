63e6e8230dc6b86fc807b293ef3434c56ccdb2c0
Gregor Werum <gregor.werum@web.de>
2020-06-07 19:07:54 +0200

    Add Frontier() class, fix remaining bugs

--- src/student_code.py
+++ src/student_code.py
@@ -1,6 +1,44 @@
-from heapq import heappop, heappush
+from heapq import heappop, heappush, heapify
 from math import sqrt
 import pdb
+
+class Frontier():
+    """docstring for Frontier"""
+    def __init__(self):
+        self.frontier = []
+        self.cities = set()
+
+    def __repr__(self):
+        return str(self.frontier)
+
+    def push(self, total_cost, path_cost, path):
+        self.cities.add(path[-1])
+        heappush(self.frontier, (total_cost, path_cost, path))
+
+    def pop(self, index = None):
+        if not index:
+            total_cost, path_cost, path = heappop(self.frontier)
+        else:
+            total_cost, path_cost, path = self.frontier[index]
+            self.frontier[index] = self.frontier[-1]
+            self.frontier.pop()
+            heapify(self.frontier)
+        popped_city = path[-1]
+        self.cities.remove(popped_city)
+        return popped_city, total_cost, path_cost, path
+
+    def lookup(self, city):
+        index = 0
+        for total_cost, path_cost, path in self.frontier:
+            if path[-1] == city:
+                return index, total_cost, path_cost, path
+            index += 1
+        return None, None, None
+
+    def exists(self, city):
+        if city in self.cities:
+            return True
+        return False
 
 class RoutePlanner():
     """docstring for RoutePlanner"""
@@ -13,49 +51,52 @@ class RoutePlanner():
         dist_x, dist_y = pos_A[0]-pos_B[0], pos_A[1]-pos_B[1]
         return sqrt(dist_x**2 + dist_y**2)
 
-    def compute_step_cost(self, current_city, next_city):
-        g_path_cost = self.compute_distance(current_city, next_city)
-        h_estimated_dist_to_target = self.compute_distance(current_city, self.target)
-        f_step_cost = g_path_cost + h_estimated_dist_to_target
-        return f_step_cost
+    def estimate_step_cost(self, current_city, next_city):
+        step_cost = self.compute_distance(current_city, next_city)
+        h_residual_cost = self.compute_distance(next_city, self.target)
+        return step_cost, h_residual_cost
 
     def compute_shortest_path(self, start_city, target_city):
-        self.frontier = []
-        self.explored = []
-        self.target = None
+        self.loop_count = 0
+        # Initialize target, explore list and frontier list
         self.target = target_city
-        current_city = start_city
-        self.explored.append(current_city)
-        current_cost = 0
-        current_path = [start_city]
-        heappush(self.frontier, (current_cost, current_path))
+        self.frontier = Frontier()
+        self.explored = [start_city]
+        # Initialize loop variables
+        current_city, current_path_cost, current_path = start_city, 0, [start_city]
 
-        while current_city != target_city:
-            if target_city == 24:
-                self.print_preliminary_results(current_city, current_path, current_cost)
+        while current_city != self.target:
+            self.print_preliminary_results(current_city, current_path, current_path_cost)
+
+            for neighbour_city in self.neighbours[current_city]:
+                if neighbour_city not in self.explored:
+                    # Estimate total cost
+                    step_cost, residual_cost = self.estimate_step_cost(current_city, neighbour_city)
+                    path_cost = current_path_cost + step_cost
+                    total_cost = path_cost + residual_cost # f = g + h
+                    neighbour_path = current_path + [neighbour_city]
+                    # Check if neighbour already exists in frontier list
+                    if self.frontier.exists(neighbour_city):
+                        f_index, f_total_cost, f_path_cost, f_path = self.frontier.lookup(neighbour_city)
+                        if (path_cost < f_path_cost):
+                            self.frontier.pop(f_index)
+                            self.frontier.push(total_cost, path_cost, neighbour_path)
+                    else:
+                        self.frontier.push(total_cost, path_cost, neighbour_path)
             
-            removed_current_city_from_frontier = heappop(self.frontier)
+            current_city, current_total_cost, current_path_cost, current_path = self.frontier.pop()
+            self.explored.append(current_city)
 
-            for neighbour in self.neighbours[current_city]:
-                if neighbour not in self.explored:
-                    self.explored.append(neighbour)
-                    step_cost = self.compute_step_cost(current_city, neighbour)
-                    new_path_cost = current_cost + step_cost
-                    new_path = current_path + [neighbour]
-                    heappush(self.frontier, (new_path_cost, new_path))
-
-            current_cost, current_path = self.frontier[0]
-            current_city = current_path[-1]
-
-
-            #pdb.set_trace()
-        return current_cost, current_path
+        return current_path_cost, current_path
 
     def print_preliminary_results(self, current_city, current_path, current_cost):
+        print("\n########### Loop {} ########### ".format(self.loop_count))
         print("\nCurrent city: {}".format(current_city))
-        print("\nCurrently explored: {}".format(self.explored))
         print("\nCurrent path: {}, Current cost: {}".format(current_path, current_cost))
+        print("\nExplored cities: {}".format(self.explored))
+        print("\nFrontier cities: {}".format(self.frontier.cities))
         print("\nCurrent frontier: {}".format(self.frontier))
+        self.loop_count += 1
 
 def shortest_path(M,start,goal):
     print("shortest path called")
